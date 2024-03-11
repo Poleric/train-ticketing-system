f62c4afa0d666e3673a712a2776f2e7fb17b136d
HongJunChea <cheahongjun@gmail.com>
2024-03-11 15:27:06 +0800

    add test

--- src/main.c
+++ src/main.c
@@ -1,16 +1,25 @@
 #include <utils.h>
 #include <stdio.h>
-
+#include <string.h>
 
 
 int main() {
-    char hash[SHA256_HASH_LENGTH + 1];
-    hash[SHA256_HASH_LENGTH] = 0;
-    char* message = "Hello, World!";
-    hash_message(message, hash);
+    char hashed_password[] = "6ca13d52ca70c883e0f0bb101e425a89e8624de51db2d2392593af6a84118090";  // abc123
+    char buffer[100];
 
-    printf("Original message: %s\n", message);
-    printf("Hashed message: %s", hash);
+    while (1) {
+        printf("Please enter the password: ");
+        
+        fgets(buffer, 100, stdin);
+        buffer[strcspn(buffer, "\n")] = 0;
+
+        if (compare_digest(buffer, hashed_password) == 0) {
+            printf("Welcome!\n");
+            break;
+        }
+        else
+            fprintf(stderr, "Wrong password. Please try again!\n");
+    }
 
     return 0;
 }
