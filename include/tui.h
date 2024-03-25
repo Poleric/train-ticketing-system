c02bb034a319967f4c214c08dbfe29d4e1c541f2
Poleric <nixonleongkb@gmail.com>
2024-03-25 14:22:55 +0800

    boilerplate for tui

---
+++ include/tui.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+#ifndef TRAINTICKETINGSYSTEM_TUI_H
+#define TRAINTICKETINGSYSTEM_TUI_H
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__) || defined(__WINDOWS__)
+#include <curses.h>
+#else
+#include <ncursesw/ncurses.h>
+#endif
+
+
+#endif //TRAINTICKETINGSYSTEM_TUI_H
---
+++ src/tui.c
@@ -0,0 +1,37 @@
+#include <tui.h>
+#include <staff.h>
+#include <member.h>
+
+
+int start_menu() {
+    initscr();
+    cbreak();  // get input by chars
+    keypad(stdscr, TRUE);
+
+    getch();
+
+    endwin();
+
+    return EXIT_SUCCESS;
+}
+
+int display_member_login() {
+    return EXIT_SUCCESS;
+}
+
+
+int display_member_menu(struct Staff current_staff) {
+    return EXIT_SUCCESS;
+}
+
+int display_staff_login() {
+    return EXIT_SUCCESS;
+
+}
+
+
+int display_staff_menu(struct Member current_member) {
+    return EXIT_SUCCESS;
+}
+
+
