102cedac3c97945ad0293f5cd28ff8233101acbc
Mingguang <teoh.yongming456@gmail.com>
2024-04-11 01:26:27 +0800

    updated int staff() * Staff_List.txt needed (not yet created)

--- src/staff.c
+++ src/staff.c
@@ -1,10 +1,31 @@
 #include <staff.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int staff() {
+	FILE* staffFP;
+	staff_t staff[50];
 
-	staff_t ali;
+
+	if ((staffFP = fopen("Staff_List.txt", "r") == NULL)) {
+		return EXIT_FAILURE;
+	}
+
+	// fscanf(staffFP, "%d | %[^|] | %s | %d | %[^|] | | %lf", );
 
 	
 
 	return 0;
+}
+
+int account() {
+
+}
+
+int schedule() {
+
+}
+
+int manageAcc() {
+
 }
