9d3c3b7b4b146a9c3cd6b1d4d556488b4bbe55ac
HongJunChea <cheahongjun@gmail.com>
2024-04-11 12:35:22 +0800

    nothing

--- src/member.c
+++ src/member.c
@@ -2,25 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+#define MAX_MEMBERS 100
 
 typedef struct {
 	char memberName[25];
-	char memberpassword[20];
+	char memPassword[20];
 }Member;
 
-
-
 void member(){
+	int numMember = 0;
+	bool check;
+	char memberName[25], memPassword[20];
+
 	FILE* memberfp;
 	memberfp = fopen("member.txt", "a");
 	
 	if (memberfp == NULL) {
-		printf("Error to open this file!!!");
+		printf("Error to open this file!\n");
 		exit(-1);
 	}
-	else() {
+	Member member(MAX_MEMBERS);
+	int numMember = 0;
 
-	}
+
 
 
 }
@@ -36,17 +42,20 @@ void login() {
 	scanf("%s", &memberName);
 
 	//to do check same name & pw
-	if (memberName==) {
+	if (memberName == ) {
 		printf("Username already exists.Please choose a different one.\n");
 		return;
 	}
 
 	printf("Enter password: ");
 	rewind(stdin);
-	scanf("%s", password);
+	scanf("%s", memberPassword);
+
 	printf("Sign Up successful!\n");
 
+	return 0;
 }
+
 
 void membernameExists() {
 	char memberName[25], memberpassword[20];
