eb7f23d5a4489586327d06fd28a39f01bc1558a8
Mingguang <teoh.yongming456@gmail.com>
2024-04-13 23:48:03 +0800

    idk what i did, but i did fscanf shit

--- src/staff.c
+++ src/staff.c
@@ -4,17 +4,18 @@
 
 int staff() {
 	FILE* staffFP;
-	staff_t staff[50];
+	staff_t currentStaff;
 
 
-	if ((staffFP = fopen("Staff_List.txt", "r") == NULL)) {
+	if ((staffFP = fopen("Staff_List.txt", "r")) == NULL) {
 		return EXIT_FAILURE;
 	}
+	else {
+		// fscanf(staffFP, "%d | %[^|] | %s | %d | %[^|] | | %lf",
+		// currentStaff.id, currentStaff.name, currentStaff.email, currentStaff.contactNo, currentStaff.position, currentStaff.permissions, scurrentStaff.alary);
 
-	// fscanf(staffFP, "%d | %[^|] | %s | %d | %[^|] | | %lf", );
 
-	
-
+	}
 	return 0;
 }
 
