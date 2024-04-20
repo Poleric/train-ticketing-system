82e5c7386221897216970b7ee44b7759921e1c31
Poleric <nixonleongkb@gmail.com>
2024-04-21 01:07:18 +0800

    update member menu to use member.c and new login_menu.c things

--- include/tui/member_menu.h
+++ include/tui/member_menu.h
@@ -5,6 +5,7 @@
 #include <tui/tui_common.h>
 #include <member.h>
 
-current_menu_t member_menu(WINDOW* menu_window);
+current_menu_t member_login_menu(WINDOW* menu_window);
+current_menu_t member_menu(WINDOW* menu_window, member_t* member);
 
 #endif //TRAINTICKETINGSYSTEM_MEMBER_MENU_H
--- src/tui/member_menu.c
+++ src/tui/member_menu.c
@@ -1,40 +1,56 @@
 #include <tui/member_menu.h>
 #include <tui/template/login_menu.h>
-#include <stdlib.h>
+#include <member.h>
 
-void after_login(char *username, char* password) {
-    printw("Logged in as: %s, with password: %s", username, password);
-}
-
-
-current_menu_t member_menu(WINDOW* menu_window) {
+// member_login -> member_menu   --┐
+//   / \        -> register_menu --|
+//    └----------------------------┘
+current_menu_t member_login_menu(WINDOW* menu_window) {
     keypad(menu_window, TRUE);
 
     LOGIN_FORM* login_form = create_login_form(menu_window);
 
-    bool exit = FALSE;
-    login_form_action_t action;
-    char* username, *password;
-    while (!exit) {
+    current_menu_t current_menu = MEMBER_MENU;
+
+    member_vector_t* members = init_members_vector();
+    load_members(members);
+
+    member_t* current_member;
+    while (current_menu == MEMBER_MENU) {
         int ch = wgetch(menu_window);
-        action = form_driver(login_form, ch);
-        switch (action) {
+
+        switch (form_driver(login_form, ch)) {
             case SUBMIT_ACTION:
-                if ((strcmp(login_form->field_buffers[0], "abc") == 0 && strcmp(login_form->field_buffers[1], "abc") == 0) ||
-                    (strcmp(login_form->field_buffers[0], "MINORI") == 0 && strcmp(login_form->field_buffers[1], "MINORI") == 0)) {
-                    username = strdup(login_form->field_buffers[0]);
-                    password = strdup(login_form->field_buffers[1]);
-                    exit = TRUE;
-                } else {
+                current_member = login_as(members, get_username(login_form), get_password(login_form));
+                if (current_member) {
+
+                    reset_login_form(login_form);
+                    wclear(menu_window);
+
+                    current_menu = member_menu(menu_window, current_member);
+
+                    print_form(login_form);
+                    wrefresh(menu_window);
+                }
+                else {
                     store_last_pos(menu_window);
-                    mvwprintw(menu_window, 3, 0, "Wrong password");
+                    mvwprintw(menu_window, 3, 0, "Wrong username or password");
                     restore_last_pos(menu_window);
                 }
                 break;
 
             case SWITCH_MENU_ACTION:
+                current_menu = STAFF_MENU;
+                break;
+
             case EXIT_FORM_ACTION:
-                exit = TRUE;
+                current_menu = EXIT_MENU;
+                break;
+
+            case RELOAD_ACTION:
+                for (int i = 0; i < members->num_of_members; i++)
+                    free_member(members->array[i]);
+                load_members(members);
                 break;
 
             default:
@@ -43,25 +59,21 @@ current_menu_t member_menu(WINDOW* menu_window) {
     }
 
     cleanup_login_form(login_form);
-
-    if (action == SUBMIT_ACTION) {
-        if (strcmp(username, "MINORI") == 0)
-            addstr(MINORI);
-        else
-            after_login(username, password);
-        refresh();
-        getch();
-        free(username);
-        free(password);
-    } else if (action == SWITCH_MENU_ACTION) {
-        printw("Switched menu");
-        refresh();
-        getch();
-        return STAFF_MENU;
-    } else if (action == EXIT_FORM_ACTION) {
-        return EXIT_MENU;
-    }
+    free_members_vector(members);
 
     keypad(menu_window, FALSE);
-    return MEMBER_MENU;
+    return current_menu;
+}
+
+current_menu_t member_menu(WINDOW* menu_window, member_t* member) {
+    current_menu_t current_menu = MEMBER_MENU;
+
+    wprintw(menu_window, "Welcome %s", member->username);
+    wrefresh(menu_window);
+
+    if (wgetch(menu_window) == KEY_ESC)
+        current_menu = EXIT_MENU;
+
+    wclear(menu_window);
+    return current_menu;
 }
