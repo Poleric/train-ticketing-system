0d78dfe3e3652b04d4530f127bceb9dcd8528ca2
Poleric <nixonleongkb@gmail.com>
2024-04-20 23:13:13 +0800

    add declaration to header

--- include/schedule/graph.h
+++ include/schedule/graph.h
@@ -23,4 +23,21 @@ struct StationGraph {
     struct StationNode** stations;
 };
 
+struct StationNode* init_node(int id, const char* station_id, const char* station_name);
+struct StationGraph* init_graph();
+
+struct StationNode* copy_node(struct StationNode *node);
+struct StationNode* get_next_node(struct StationNode* node);
+
+int node_add_next_connection(struct StationNode* start_node, struct StationNode* next_node, int distance);
+int node_add_connection(struct StationNode* start_node, struct StationNode* next_node, int distance);
+
+struct StationNode* graph_get_node(struct StationGraph* graph, const char* station_id);
+int resize_graph(struct StationGraph *graph);
+int graph_add_node(struct StationGraph* graph, const char* station_id, const char* station_name);
+int graph_add_connection(struct StationGraph* graph, const char* station_id, const char* next_station_id, int distance);
+
+void free_node_list(struct StationNode* node);
+void free_graph(struct StationGraph* graph);
+
 #endif //TRAINTICKETINGSYSTEM_GRAPH_H
