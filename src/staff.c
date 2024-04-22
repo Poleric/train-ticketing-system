93f8e1a2045f662565a683f44d80f3674716fb27
Mingguang <teoh.yongming456@gmail.com>
2024-04-23 01:06:14 +0800

    I think load_staff() is done...?

--- src/staff.c
+++ src/staff.c
@@ -1,21 +1,32 @@
 #include <staff.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int staff() {
+#define BASE_STAFF_LENGTH 4
+
+int load_staff() {
 	FILE* staffFP;
 	staff_t currentStaff;
 
-
 	if ((staffFP = fopen("Staff_List.txt", "r")) == NULL) {
+		fprintf(stderr, "Error to open this file!\n");
 		return EXIT_FAILURE;
 	}
-	else {
-		// fscanf(staffFP, "%d | %[^|] | %s | %d | %[^|] | | %lf",
-		// currentStaff.id, currentStaff.name, currentStaff.email, currentStaff.contactNo, currentStaff.position, currentStaff.permissions, scurrentStaff.alary);
 
+	fscanf(staffFP, "%[^|] | %[^|] | %s | %d | %[^|] | %d | %lf",
+	currentStaff.username, currentStaff.hashed_password, currentStaff.email, currentStaff.contactNo, currentStaff.position, currentStaff.permissions, scurrentStaff.salary);
 
+	char username_buffer[255], password_buffer[255];
+	char retry;
+
+	if ((strcmp(username_buffer, currentStaff.username) || strcmp(password_buffer, currentStaff.hashed_password)) != 0) {
+		printf("Wrong username and password combination. Want to try again? (Y to retry) > ");
+		scanf("%c", retry);
+		return EXIT_FAILURE;
 	}
+
 	return 0;
 }
 
