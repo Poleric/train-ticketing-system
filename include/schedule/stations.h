448ab22d9dd13e45b131dd783652ed3dcab33023
Poleric <nixonleongkb@gmail.com>
2024-04-23 20:01:32 +0800

    directly put station id and name in node

--- include/schedule/stations.h
+++ include/schedule/stations.h
@@ -5,7 +5,6 @@
 
 typedef struct StationNode station_node_t;
 typedef struct Edge edge_t;
-typedef struct StationDetails station_details_t;
 typedef struct StationGraph station_graph_t;
 
 typedef struct SerializedEdge s_serialized_edge;
@@ -15,14 +14,9 @@ struct Edge {
     int distance;
 };
 
-struct StationDetails {
+struct StationNode {
     char* station_id;
     char* station_name;
-};
-
-struct StationNode {
-    int id;
-    station_details_t details;
     edge_t edge;
 };
 
@@ -32,7 +26,7 @@ struct StationGraph {
     station_node_t** nodes;
 };
 
-station_node_t* init_node(int id, const char* station_id, const char* station_name);
+station_node_t* init_node(char* station_id, char* station_name);
 station_graph_t* init_graph();
 
 station_node_t* copy_node(station_node_t *node);
@@ -44,8 +38,9 @@ int node_add_connection(station_node_t* start_node, station_node_t* next_node, i
 station_node_t* graph_get_node(station_graph_t* graph, const char* station_id);
 int resize_graph(station_graph_t *graph);
 int graph_add_node(station_graph_t* graph, const char* station_id, const char* station_name);
-int graph_add_connection_bidirectional(station_graph_t* graph, const char* station_id, const char* next_station_id, int distance);
-int graph_add_connection_directional(station_graph_t* graph, const char* station_id, const char* next_station_id, int distance);
+
+int graph_add_connection_bidirectional(station_graph_t* graph, char* station_id, char* next_station_id, int distance);
+int graph_add_connection_directional(station_graph_t* graph, char* station_id, char* next_station_id, int distance);
 
 void free_node_list(station_node_t* node);
 void free_graph(station_graph_t* graph);
--- src/schedule/stations.c
+++ src/schedule/stations.c
@@ -3,13 +3,10 @@
 #include <string.h>
 
 
-station_node_t* init_node(int id, const char* station_id, const char* station_name) {
+station_node_t* init_node(char* station_id, char* station_name) {
     station_node_t* node = malloc(sizeof (station_node_t));
-    node->id = id;
-    node->details = (station_details_t){
-            strdup(station_id),
-            strdup(station_name)
-    };
+    node->station_id = station_id;
+    node->station_name = station_name;
 
     return node;
 }
@@ -24,7 +21,7 @@ station_graph_t* init_graph() {
 }
 
 station_node_t* copy_node(station_node_t *node) {
-    return init_node(node->id, node->details.station_id, node->details.station_name);
+    return init_node(node->station_id, node->station_name);
 }
 
 station_node_t* get_next_node(station_node_t* node) {
@@ -48,7 +45,7 @@ int node_add_connection(station_node_t* start_node, station_node_t* next_node, i
 
 station_node_t* graph_get_node(station_graph_t* graph, const char* station_id) {
     for (int i = 0; i < graph->number_of_nodes; i++)
-        if (strcmp(graph->nodes[i]->details.station_id, station_id) == 0)
+        if (strcmp(graph->nodes[i]->station_id, station_id) == 0)
             return graph->nodes[i];
     return NULL;
 }
@@ -69,11 +66,11 @@ int graph_add_node(station_graph_t* graph, const char* station_id, const char* s
         if (resize_graph(graph) == EXIT_FAILURE)
             return EXIT_FAILURE;
     }
-    graph->nodes[graph->number_of_nodes++] = init_node(index++, station_id, station_name);
+    graph->nodes[graph->number_of_nodes++] = init_node(strdup(station_id), strdup(station_name));
     return EXIT_SUCCESS;
 }
 
-int graph_add_connection_bidirectional(station_graph_t* graph, const char* station_id, const char* next_station_id, int distance) {
+int graph_add_connection_bidirectional(station_graph_t* graph, char* station_id, char* next_station_id, int distance) {
     station_node_t* node = graph_get_node(graph, station_id);
     station_node_t* next_node = graph_get_node(graph, next_station_id);
 
@@ -83,7 +80,7 @@ int graph_add_connection_bidirectional(station_graph_t* graph, const char* stati
     return EXIT_SUCCESS;
 }
 
-int graph_add_connection_directional(station_graph_t* graph, const char* station_id, const char* next_station_id, int distance) {
+int graph_add_connection_directional(station_graph_t* graph, char* station_id, char* next_station_id, int distance) {
     station_node_t* node = graph_get_node(graph, station_id);
     station_node_t* next_node = graph_get_node(graph, next_station_id);
 
@@ -93,10 +90,12 @@ int graph_add_connection_directional(station_graph_t* graph, const char* station
 }
 
 void free_node_list(station_node_t* node) {
+    printf("Freeing %s\n", node->station_id);
+    free(node->station_id);
+    free(node->station_name);
     while (node) {
         station_node_t* next = get_next_node(node);
-        free(node->details.station_id);
-        free(node->details.station_name);
+        printf("%p\n", node);
         free(node);
         node = next;
     }
@@ -112,10 +111,10 @@ void free_graph(station_graph_t* graph) {
 int save_stations(station_graph_t* graph, FILE* fp) {
     fwrite(&graph->number_of_nodes, sizeof(int), 1, fp);
     for (int i = 0; i < graph->number_of_nodes; i++) {
-        fwrite(graph->nodes[i]->details.station_id, sizeof(char), 4, fp);
-        int station_name_len = (int) strlen(graph->nodes[i]->details.station_name) + 1;
+        fwrite(graph->nodes[i]->station_id, sizeof(char), 4, fp);
+        int station_name_len = (int) strlen(graph->nodes[i]->station_name) + 1;
         fwrite(&station_name_len, sizeof(int), 1, fp);
-        fwrite(graph->nodes[i]->details.station_name, sizeof (char), station_name_len, fp);
+        fwrite(graph->nodes[i]->station_name, sizeof (char), station_name_len, fp);
     }
     return EXIT_SUCCESS;
 }
@@ -148,8 +147,8 @@ int save_connections(station_graph_t* graph, FILE* fp) {
         while (next_node) {
             s_serialized_edge edge;
             edge.distance = node->edge.distance;
-            strncpy(edge.from_station_id, node->details.station_id, 4);
-            strncpy(edge.to_station_id, next_node->details.station_id, 4);
+            strncpy(edge.from_station_id, node->station_id, 4);
+            strncpy(edge.to_station_id, next_node->station_id, 4);
 
             fwrite(&edge, sizeof(s_serialized_edge), 1, fp);
             n_of_connections++;
@@ -179,7 +178,7 @@ int save_graph(station_graph_t* graph, const char* filepath) {
     if (fp == NULL)
         return EXIT_FAILURE;
 
-    save_locations(graph, fp);
+    save_stations(graph, fp);
     save_connections(graph, fp);
     return EXIT_SUCCESS;
 }
@@ -190,7 +189,7 @@ int load_graph(station_graph_t* graph, const char* filepath) {
     if (fp == NULL)
         return EXIT_FAILURE;
 
-    load_locations(graph, fp);
+    load_stations(graph, fp);
     load_connections(graph, fp);
     return EXIT_SUCCESS;
 }
