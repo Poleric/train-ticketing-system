3e85f00f4e334a5b9ae02cf09f4000c029220954
Poleric <nixonleongkb@gmail.com>
2024-04-25 10:18:36 +0800

    update header file

--- include/tui/scheduling_menu.h
+++ include/tui/scheduling_menu.h
@@ -20,6 +20,17 @@ struct Table {
     WINDOW* pad;
 };
 
+void move_to_next_line(WINDOW* window, int x);
+void move_to_x(WINDOW* window, int x);
+void move_offset_x(WINDOW* window, int offset_x);
+void print_header(table_t* table);
+void print_footer(table_t* table);
+char* to_time(dt_time_t time, char* buffer, int n);
+void print_schedule_row(table_t* table, schedule_t * schedule);
+void display_schedules(table_t* table, schedule_vector_t* schedule_vector);
+void init_schedule_table(table_t* table, int n_lines, int column_padding);
+void free_schedule_table(table_t* table);
+
 void schedule_menu();
 
 #endif //TRAINTICKETINGSYSTEM_SCHEDULING_MENU_H
