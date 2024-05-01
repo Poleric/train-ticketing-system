f558bfa49f05de8560431adac521da878fdcbbe6
Poleric <nixonleongkb@gmail.com>
2024-05-01 23:36:09 +0800

    cleanup menu utils

--- src/tui/utils/menu_utils.c
+++ src/tui/utils/menu_utils.c
@@ -1,16 +1,6 @@
 #include <tui/utils/menu_utils.h>
 
-void clear_current_menu(WINDOW* menu_window, FORM* form) {
-    reset_form(form);
-    wclear(form->window);
-    wclear(menu_window);
-}
-
-void display_login_menu(const char* header_text, WINDOW* menu_window, FORM* login_form) {
-    // header
-    mvwprintw(menu_window, 0, get_centered_x_start(menu_window, (int)strlen(header_text)), "%s", header_text);
-    wrefresh(menu_window);
-    // form
-    print_login_form(login_form);
-    wrefresh(login_form->window);
+void clear_login_menu(login_form_t* login_form) {
+    reset_form(&login_form->form);
+    wclear(login_form->form.window);
 }
