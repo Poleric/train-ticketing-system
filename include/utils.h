d2e47218db05de8be5a689387ee67aee520fa64c
Poleric <nixonleongkb@gmail.com>
2024-05-01 13:35:00 +0800

    add tm_wday constants as enum

--- include/utils.h
+++ include/utils.h
@@ -8,6 +8,17 @@
 #define SHA256_HASH_LENGTH 64
 
 typedef struct datetime_time dt_time_t;
+typedef enum tm_wday tm_wday_t;
+
+enum tm_wday {
+    SUNDAY = 0,
+    MONDAY = 1,
+    TUESDAY = 2,
+    WEDNESDAY = 3,
+    THURSDAY = 4,
+    FRIDAY = 5,
+    SATURDAY = 6
+};
 
 struct datetime_time {
     int tm_hour;
@@ -24,7 +35,7 @@ void flush(FILE* stream);
 void delete_char(char* buffer, int pos);
 void add_char(char* buffer, char ch, int pos);
 const char* tm_mon_to_text(int tm_mon);
-const char* tm_wday_to_text(int tm_wday);
+const char* tm_wday_to_text(tm_wday_t tm_wday);
 char* to_time(dt_time_t time, char* buffer, int n);
 
 #endif //TRAINTICKETINGSYSTEM_UTILS_H
