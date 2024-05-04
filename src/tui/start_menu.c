8f8dfc58bd97a688ca7feb3f59e7c69374cf1bc0
Poleric <nixonleongkb@gmail.com>
2024-05-04 10:13:30 +0800

    fix cleanup 2

--- src/tui/start_menu.c
+++ src/tui/start_menu.c
@@ -192,10 +192,6 @@ void member_menu(WINDOW* menu_window, member_t* member) {
                         break;
                 }
 
-                // uh cleanup somehow affect member_menu_window
-                keypad(member_menu_window, true);
-                curs_set(0);
-
                 if (!exit) display_member_menu(&member_menu, COLOR_1);
                 break;
 
@@ -210,6 +206,10 @@ void member_menu(WINDOW* menu_window, member_t* member) {
             default:
                 break;
         }
+
+        // uh cleanup somehow affect member_menu_window
+        keypad(member_menu_window, true);
+        curs_set(0);
     }
 
     free_member_menu(&member_menu);
